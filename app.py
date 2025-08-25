90735313f35ad479bf260bff3968e361dc012263
yasmin-21189 <barcelos.batista@escola.pr.gov.br>
2025-08-25 18:05:29 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -4,12 +4,16 @@ import pandas as pd
 st.title("Painel de Atendimento Médico")
 
 # Upload do CSV
-uploaded_file = st.file_uploader("atendimentos.csv", type="csv")
-if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file, sep=';', encoding='latin-1')
-    df.columns = df.columns.str.strip()
+uploaded_file = st.file_uploader("atendimento.csv", type="csv")
 
-    st.write("Dados carregados com sucesso!")
-    st.dataframe(df.head())
+if uploaded_file is not None:
+    # Lê o CSV
+    df = pd.read_csv(uploaded_file, sep=';', encoding='latin-1')
+    df.columns = df.columns.str.strip()  # remove espaços extras nos nomes das colunas
+    
+    st.success("CSV carregado com sucesso!")
+    
+    # Mostrar os dados
+    st.dataframe(df.head())  # mostra as primeiras linhas
 else:
     st.warning("Faça upload do arquivo CSV para visualizar os dados.")
